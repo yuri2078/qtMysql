9296bf2b5442ef5358016f937fcb6de3aa246ef7
yuri2078 <yuri2078170658@gmail.com>
2023-04-06 23:03:40 +0800

    main

--- src/note.cpp
+++ src/note.cpp
@@ -2,6 +2,9 @@
 
 #include <QFileDialog>
 #include <QKeyEvent>
+#include <QTextBlock>
+#include <qnamespace.h>
+#include <qtextcursor.h>
 
 Note::Note(QWidget *parent) : QTextEdit(parent) {
   QFile file(":css/note.css");
@@ -11,6 +14,7 @@ Note::Note(QWidget *parent) : QTextEdit(parent) {
   move(50, 50);
 }
 
+// 打开一个文件对话框并保存文件
 void Note::saveFile() {
   QString file_name = QFileDialog::getSaveFileName(this, "请打开一个文件", "/home/yuri", "");
   QFile file(file_name, this);
@@ -23,6 +27,7 @@ void Note::saveFile() {
   file.close();
 }
 
+// 打开一个文件对话框并打开文件
 void Note::openFile() {
   QString file_name = QFileDialog::getOpenFileName(this, "请选择保存的位置", "/home/yuri", "");
   QFile file(file_name, this);
@@ -35,10 +40,46 @@ void Note::openFile() {
   file.close();
 }
 
+// 重写键盘事件
 void Note::keyPressEvent(QKeyEvent *event) {
+  QTextCursor currentCursor = this->textCursor();
+  QString line_text = textCursor().block().text();
+
   if (event->key() == Qt::Key_Tab) {
-    insertPlainText(" ");
-  } else {
+    insertPlainText("  ");
+    event->accept(); // 标记为已经处理
+  } else if (event->key() == Qt::Key_Return) {
+    // 获取本行文本
+    
+    int count_space = 0;
+    for (QChar c : line_text) {
+      if (c.isSpace()) {
+        count_space++;
+      } else {
+        break;
+      }
+    }
+
+    if (line_text.count('{') > line_text.count('}')) {
+
+      QString str = "\n" + QString(' ').repeated(count_space + 2) + "\n" + QString(' ').repeated(count_space) + "}";
+      
+      currentCursor.insertText(str);
+      currentCursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::MoveAnchor);
+      currentCursor.movePosition(QTextCursor::Up, QTextCursor::MoveAnchor, 1);
+      currentCursor.movePosition(QTextCursor::Right, QTextCursor::MoveAnchor, count_space + 2);
+      
+    } else {
+      insertPlainText("\n" + QString(' ').repeated(count_space));
+    }
+
+    event->accept();
+  } else if (event->key() == Qt::Key_ParenLeft) {
+    insertPlainText("()");
+    currentCursor.movePosition(QTextCursor::Left, QTextCursor::MoveAnchor, 1);
+  }
+  else {
     QTextEdit::keyPressEvent(event);
   }
+  this->setTextCursor(currentCursor);
 }
